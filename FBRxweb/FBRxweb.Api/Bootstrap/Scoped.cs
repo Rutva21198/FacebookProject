5be67119a2b5edaa295ca49543f2b930597f1d2e
Rutva Gajera <rutwapatel.99@gmail.com>
2020-02-25 13:43:19 +0530

    Final Project And Repository add.

---
+++ FBRxweb/FBRxweb.Api/Bootstrap/Scoped.cs
@@ -0,0 +1,51 @@
+#region Namespace
+using Microsoft.Extensions.DependencyInjection;
+using FBRxweb.Infrastructure.Security;
+using RxWeb.Core.Data;
+using RxWeb.Core.Security;
+using RxWeb.Core.Annotations;
+using RxWeb.Core;
+using FBRxweb.UnitOfWork.DbEntityAudit;
+using FBRxweb.BoundedContext.Main;
+using FBRxweb.UnitOfWork.Main;
+#endregion Namespace
+
+
+
+namespace FBRxweb.Api.Bootstrap
+{
+    public static class ScopedExtension
+    {
+
+        public static void AddScopedService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IRepositoryProvider, RepositoryProvider>();
+            serviceCollection.AddScoped<ITokenAuthorizer, TokenAuthorizer>();
+            serviceCollection.AddScoped<IModelValidation, ModelValidation>();
+            serviceCollection.AddScoped<IAuditLog, AuditLog>();
+            serviceCollection.AddScoped<IApplicationTokenProvider, ApplicationTokenProvider>();
+            serviceCollection.AddScoped(typeof(IDbContextManager<>), typeof(DbContextManager<>));
+
+            #region ContextService
+
+            serviceCollection.AddScoped<ILoginContext, LoginContext>();
+            serviceCollection.AddScoped<ILoginUow, LoginUow>();
+             
+            serviceCollection.AddScoped<IFacebookContext, FacebookContext>();
+            serviceCollection.AddScoped<IFacebookUow, FacebookUow>();
+            #endregion ContextService
+
+
+
+
+
+            #region DomainService
+
+            #endregion DomainService
+        }
+    }
+}
+
+
+
+
